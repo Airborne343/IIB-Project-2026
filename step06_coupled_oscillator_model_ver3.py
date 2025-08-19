7fda41dbc4c6a7edabd274e663cff3d2e7dd52a0
Rohan Babu <>
2025-08-19 16:33:51 +0100

    new understanding of Vh applied

--- step06_coupled_oscillator_model_ver3.py
+++ step06_coupled_oscillator_model_ver3.py
@@ -42,29 +42,19 @@ omega_sweep = [freq * (2 * np.pi) for freq in frequency]
 
 _, _, _, A, B, C, D = state_space_model(all_poles, all_residues, d)
 
-#__Alpha_Function__
-def alpha_function(Z_d_val):
-    trachea_area = area[33]
-    psi_mag_squared = np.abs(psi_j) ** 2
-    psi_norm_squared = np.trapz(np.abs(psi)**2, dx=1)
-
-    if np.abs(Z_d_val) < 1e-10:
-        print(f"Z_d too small or invalid at this step: {Z_d_val}")
-        return 0.0
-    
-    alpha = -(rho * (c_gas**2) * psi_mag_squared * trachea_area)/(psi_norm_squared * Z_d_val)
-    return alpha
-
 def coupled_oscillatory_model(t, state):
     eta, eta_dot, u, u_dot = state[0], state[1], state[2], state[3]  #state space vector
     x = state[4 :]
     
     #parameters
-    omega_d = c_gas * np.sqrt(area[33]/(V_h(t) * l_m[33]))   #damper frequency
-    nu = 50                                                  #growth rate
+    nu_const = 1
+    nu = nu_const * U(t)                                     #growth rate
     kappa = 1000                                             #non-linear coefficient
     beta = zeta/l_m[33]                                      #u_dot term in 2nd eqn
     gamma = - psi[0]/(rho * l_m[33])                         #eta_dot term in 2nd eqn
+    trachea_area = area[33]
+    psi_mag = np.abs(psi_j)
+    psi_norm_squared = np.trapz(np.abs(psi)**2, dx=1)
     
     x = np.array(x).reshape(-1, 1)
     u_input = np.array([[u]])
@@ -73,13 +63,12 @@ def coupled_oscillatory_model(t, state):
     
     #Z_t equation
     Z_t = np.real(C @ x + D * u_input)
-    alpha = alpha_function(Z_t)
     
     #eta_ddot equation
-    eta_ddot = - ((np.abs(omega_0) ** 2) * eta) - (alpha * eta_dot) + (2 * nu * eta_dot) - (kappa * (eta ** 2) * eta_dot)
+    eta_ddot = - ((np.abs(omega_0) ** 2) * eta) - ((-(rho * (c_gas**2) * psi_mag * trachea_area)/(psi_norm_squared)) * u_dot) + (2 * nu * eta_dot) - (kappa * (eta ** 2) * eta_dot)
     
     #u_ddot equation
-    u_ddot = (-beta * (float(U(t)) + u) * u_dot - (abs(float(dU(t))) * beta + omega_d**2) * u - gamma * eta_dot)
+    u_ddot = (-beta * (float(U(t)) + u) * u_dot - (abs(float(dU(t))) * beta + ((c_gas**2 * area[33])/(V_h(t)*l_m[33]))) * u - gamma * eta_dot)
     
     return [float(np.real(eta_dot).item()), float(np.real(eta_ddot).item()), float(np.real(u_dot).item()), float(np.real(u_ddot).item())] + np.real(x_dot).flatten().tolist()
 
